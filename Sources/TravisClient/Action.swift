614d4badd397a440286a6b6948d145b2b48e44f1
Iain <iain@mountain23.com>
2018-04-15 20:41:19 +0100

    Improve naming

--- Sources/TravisClient/Action.swift
+++ Sources/TravisClient/Action.swift
@@ -2,22 +2,22 @@ import Foundation
 
 public struct Action<Object: Codable>: Codable, ObjectSubscriptable {
     public let type: String
-    public let resource_type: String
-    public let state_change: String
+    public let resourceType: String
+    public let stateChange: String
     public let object: Object
 
     enum CodingKeys: String, CodingKey {
         case type = "@type"
-        case resource_type
-        case state_change
+        case resourceType = "resource_type"
+        case stateChange = "state_change"
     }
 
     public init(from decoder: Decoder) throws {
         let standardContainer = try decoder.container(keyedBy: CodingKeys.self)
         type = try standardContainer.decode(String.self, forKey: .type)
-        let resource_type = try standardContainer.decode(String.self, forKey: .resource_type)
-        self.resource_type = resource_type
-        state_change = try standardContainer.decode(String.self, forKey: .state_change)
+        let resource_type = try standardContainer.decode(String.self, forKey: .resourceType)
+        self.resourceType = resource_type
+        stateChange = try standardContainer.decode(String.self, forKey: .stateChange)
         let dynamicContainer = try decoder.container(keyedBy: DynamicKey.self)
         object = try dynamicContainer.decode(Object.self, forKey: DynamicKey(stringValue: resource_type)!)
     }
